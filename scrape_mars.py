98a29751d17fb37467e921daa4a7f9441190fdac
maxe23 <maxeiteljorg@gmail.com>
2021-05-05 00:10:23 -0400

    added scrape info

--- scrape_mars.py
+++ scrape_mars.py
@@ -9,3 +9,18 @@ from webdriver_manager.chrome import ChromeDriverManager
 def init_browser():
     executable_path = {"executable_path": ChromeDriverManager().install()}
     return Browser("chrome", **executable_path, headless = False)
+def scrape():
+    browser = init_browser()
+    mars = {}
+
+    url = 'https://mars.nasa.gov/news/'
+    browser.visit(url)
+
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
+
+    mars['news_title'] = soup.find_all("div", class_ = "content_title")[1].text
+    mars['news_paragraph'] = soup.find("div", class_ = "article_teaser_body").text
+
+    url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
+    browser.visit(url)  
